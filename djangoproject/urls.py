067f6094bc0f99f68726621614b32721ada1d202
pedrasfloki <pedrasfloki@gmail.com>
2021-05-01 03:57:31 +0100

    using FilterView

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django_filters.views import FilterView
 from products import views as product_views
+from products.filters import ProductFilter
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # in this url we'll display and filter products list 
-    path('products/', product_views.product_list, name='products'),
+    # use one of the following options 
+    # path('products/', product_views.product_list, name='products'),
+    path('products/', FilterView.as_view(
+        filterset_class=ProductFilter, template_name='products/product_list.html'), name='products'),
 ]
 
 
